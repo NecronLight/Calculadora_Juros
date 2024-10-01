25ae466dcb543c5162c4e16fab39c329ef3568f4
Lucas <lucasluz13@hotmail.com>
2024-10-01 18:39:47 -0300

    Comentários adicionados

--- juros.py
+++ juros.py
@@ -1,14 +1,15 @@
+#Pede para que o usuário insira o valor da dívida. O valor é convertido em 'float'
 divida = float(input("Qual o valor da dívida? "))
 print("")
 
-
+#Informa a quantidade de parcelas, taxa de juros, dívida atualizada e o valor de cada parcela caso o pagamento seja à vista.
 print("Quantidade de parcelas: 1")
 print("Taxa de juros sobre o valor inicial da dívida: 0%")
 print("A dívida atualizada é de R$: ", divida)
 print("Valor de cada parcela R$: ", divida)
 print("")
 
-
+#Informa a taxa de juros, dívida atualizada e o valor de cada parcela conforme o número de parcelas (3, 6, 9 ou 12)
 qnt_parcelas = 0
 for qnt_parcelas in range(3, 13, 3):
     print("Quantidade de parcelas: ",qnt_parcelas)
